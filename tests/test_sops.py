70d775d8ab17a4f5f1365aa6475662e5c47be0da
Pavel Zwerschke <pavelzw@gmail.com>
2024-10-09 20:29:54 +0200

    Add JSON test

--- tests/test_sops.py
+++ tests/test_sops.py
@@ -7,20 +7,52 @@ from pydantic_settings import (
 from pydantic_settings_sops import SOPSConfigSettingsSource
 
 
-def test_get_settings():
-    settings = TestSettings()
+def test_settings_yaml():
+    settings = TestSettingsYaml()
     assert settings.foobar == "foo"
     assert settings.foobar2 == "foo"
 
-    settings2 = TestSettings(foobar="bar")
+    settings2 = TestSettingsYaml(foobar="bar")
     assert settings2.foobar == "bar"
     assert settings2.foobar2 == "foo"
 
 
-class TestSettings(BaseSettings):
+def test_settings_json():
+    settings = TestSettingsJson()
+    assert settings.foobar == "foo"
+    assert settings.foobar2 == "foo"
+
+    settings2 = TestSettingsJson(foobar="bar")
+    assert settings2.foobar == "bar"
+    assert settings2.foobar2 == "foo"
+
+
+
+class TestSettingsYaml(BaseSettings):
     # todo: add json tests: https://github.com/nikaro/sopsy/issues/73
     model_config = SettingsConfigDict(
         yaml_file=["tests/resources/secrets.yaml", "tests/resources/secrets2.yaml"]
+    )
+
+    foobar: str
+    foobar2: str
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: BaseSettings,  # type: ignore[override]
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (init_settings, SOPSConfigSettingsSource(settings_cls))
+
+
+class TestSettingsJson(BaseSettings):
+    # todo: add json tests: https://github.com/nikaro/sopsy/issues/73
+    model_config = SettingsConfigDict(
+        yaml_file=["tests/resources/secrets.json", "tests/resources/secrets2.json"]
     )
 
     foobar: str
